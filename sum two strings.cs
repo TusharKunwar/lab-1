164a3518f993e4283353c0f886617079fd38892e
TusharKunwar <tusharkunwar379@gmail.com>
2024-12-10 16:53:29 +0545

    Add files via upload

---
+++ sum two strings.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Enter two strings:");
+
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+
+string result = a + b;
+
+Console.WriteLine("Final string is:" + result);
