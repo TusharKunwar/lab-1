64a3c02e2059d60c053e97fea6e5f639310fe0dd
TusharKunwar <tusharkunwar379@gmail.com>
2024-12-10 16:50:55 +0545

    Add files via upload

---
+++ sum.cs
@@ -0,0 +1,10 @@
+Console.WriteLine("Enter two numbers:");
+
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+
+int aa = Convert.ToInt32(a);
+int bb = Convert.ToInt32(b);
+
+int sum = aa + bb;
+Console.WriteLine("sum of " + a + " and " + b + " is " + sum);
