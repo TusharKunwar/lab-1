aab1aeeb459e76f10a700b702474bda71f4418a2
TusharKunwar <tusharkunwar379@gmail.com>
2024-12-10 16:58:28 +0545

    Add files via upload

---
+++ if...else.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Enter two numbers:");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1> num2) {
+    Console.WriteLine(num1+" is greater");
+}
+else{
+    Console.WriteLine(num2+" is greater");
+}
